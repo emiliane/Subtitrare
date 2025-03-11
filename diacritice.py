1653723f43e0e2e873196d4a5e1b1859b251172b
Emilian <emiliane@users.noreply.github.com>
2025-03-12 00:49:01 +0200

    Add files via upload

--- diacritice.py
+++ diacritice.py
@@ -5,16 +5,12 @@ fisier = sys.argv[1]
 codare1 = "utf-8"
 codare2 = "iso-8859-1"
 
-def citesteFisier(fname, codare):
-  f = open(fname, "r", encoding=codare)
-  return f    
-
 try:
-  f = citesteFisier(fisier, codare1)
-  s = f.read()
+    f = citesteFisier(fisier, codare1)
+    s = f.read()
 except:
-  f = citesteFisier(fisier, codare2)
-  s = f.read()
+    f = citesteFisier(fisier, codare2)
+    s = f.read()
 
 print("Citit fișier subtitrare: " + fisier)
 
@@ -25,24 +21,45 @@ s = s.replace("º", "ș")
 s = s.replace("Þ", "Ț")
 s = s.replace("þ", "ț")
 
-s = s.replace("\u00c3", "Ă") #Ă
-s = s.replace("\u00e3", "ă") #ă
-s = s.replace("\u015e", "Ș") #Ș
-s = s.replace("\u015f", "ș") #ș
-s = s.replace("\u0162", "Ț") #Ț
-s = s.replace("\u0163", "ț") #ț
+s = s.replace("\u00c3", "Ă")
+s = s.replace("\u00e3", "ă")
+s = s.replace("\u015e", "Ș")
+s = s.replace("\u015f", "ș")
+s = s.replace("\u0162", "Ț")
+s = s.replace("\u0163", "ț")
+
+s = s.replace("  ", " ")
+s = s.replace(" .", ".")
+s = s.replace(" :", ":")
+s = s.replace(" ,", ",")
+s = s.replace(" ;", ";")
+s = s.replace(" !", "!")
+s = s.replace(" ?", "?")
+s = s.replace("...", "…")
+s = s.replace("", " ")
+
+s = s.strip()
+while "  " in s:
+    s = s.replace("  ", " ")
+
+s = s.replace(" \n", "\n")
 
 punct = "."
 fisier2 = fisier.rsplit(punct, 1)
 
-try:  
-  fisierNou = fisier2[0] + "-corectat" + punct + fisier2[1]
+try:
+    fisierNou = fisier2[0] + "-corectat" + punct + fisier2[1]
 except:
-  print("Fișier fără extensie")
-  fisierNou = fisier2[0] + "-corectat"
+    print("Fișier fără extensie")
+    fisierNou = fisier2[0] + "-corectat"
 
 f = open(fisierNou, "w")
 f.write(s)
 f.close()
 
 print("Scris fișier subtitrare corectat: " + fisierNou)
+
+
+def citesteFisier(fname, codare):
+    f = open(fname, "r", encoding=codare)
+    return f
